e65e9e4f908bec9de6bff027fb8f90808b98c6fb
Mario Gomes <mariodebora2@gmail.com>
2023-02-21 20:26:53 -0300

    Mudanças implementadas!

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
-import { 
-  Center,
-  ChakraProvider,
-  Input,
-  Box,
-  Button
-} from '@chakra-ui/react'
-import { login } from './services/login';
+import { Header} from './components/Header/Header'
+import { Card } from './components/Card'
+import { Box } from '@chakra-ui/react'
+
+
 
 function App() {
   return (
-    <ChakraProvider>
-      <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
-        <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='15px' >
-          <Center>
-            <h1>Faça o login</h1>
-          </Center>
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <Center>
-            <Button onClick={login} colorScheme='teal' size='sm' width='100%' marginTop='5px'>
-              Button
-            </Button>
-          </Center>
-        </Box>
+    <>
+      <Box minHeight='100vh' backgroundColor='#242424' padding='25px'>
+        <Header></Header>
+        <Card></Card>
       </Box>
-    </ChakraProvider>
+    </>
   );
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,24 @@
-interface ICard {
-  id: number,
-  paragraph: string,
-  details: string
-}
+import { 
+  Center,
+  ChakraProvider,
+  Input,
+  Box,
+} from '@chakra-ui/react'
+import { ButtonLogin } from '../../src/components/Button'
 
-export const Card = ({ id, paragraph, details }: ICard) => {
-  return(
-    <div>
-      <h1>Card {id}</h1>
-      <p>{paragraph}</p>
-      <p>{details}</p>
-    </div>
+export const Card = (): any => {
+  return (
+    <ChakraProvider>
+        <Box backgroundColor='#141414' borderRadius='25px' padding='15px' color='#ffffff' >
+          <Center margin='10px 0' fontSize={20}>
+            <h1>Faça o login</h1>
+          </Center>
+          <Input placeholder="email" margin='10px 0'/>
+          <Input placeholder="password" margin='10px 0'/>
+          <Center>             
+            <ButtonLogin/>
+          </Center>
+        </Box>
+    </ChakraProvider>
   )
 }
